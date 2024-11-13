ba99ddbed36dfe280d6104f60c0c4e4b3d2c6149
himanushi <himanushi@users.noreply.github.com>
2024-11-14 08:15:11 +0900

    不必要なインポートの削除

--- src/client/pages/home/HomeLayout.tsx
+++ src/client/pages/home/HomeLayout.tsx
@@ -1,5 +1,3 @@
 import { Box } from "@yamada-ui/react";
-import { Me } from "~/client/features/me/Me";
-import { Todo } from "~/client/features/todo/Todo";
 
 export const HomeLayout = () => <Box>aaa</Box>;
